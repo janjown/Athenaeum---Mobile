9cd17628e1611425a8c02da1ffe2fca9a6e22c7a
John Henry C. Angas <angasjohnhenry@gmail.com>
2024-05-08 13:25:31 +0800

    updated Dropdown

--- components/dropDown_registration.js
+++ components/dropDown_registration.js
@@ -1,116 +1,10 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Dropdown } from "react-native-element-dropdown";
-
-const data = [
-  {
-    label: "Bachelor of Science in Business Administration (BSBA)",
-    value: "1",
-  },
-  { label: "Bachelor of Science in Computer Science (BSCS)", value: "2" },
-  { label: "Bachelor of Science in Information Technology (BSIT)", value: "3" },
-  { label: "Bachelor of Science in Hospitality Management (BSHM)", value: "4" },
-  { label: "Bachelor of Science in Office Administration (BSOA)", value: "5" },
-  {
-    label: "Bachelor of Science in Industrial Technology (BSIT)",
-    value: "6",
-    children: [
-      {
-        label: "Industrial Technology",
-        value: "industrial",
-        children: [
-          { label: "Sub Category 1", value: "sub1" },
-          { label: "Sub Category 2", value: "sub2" },
-        ],
-      },
-    ],
-  },
-  { label: "Bachelor of Science in Psychology (BSP)", value: "7" },
-  { label: "Bachelor of Science in Criminology (BSCRIM)", value: "7" },
-  {
-    label: "Bachelor of Science in Technology and Livelihood Education",
-    value: "8",
-  },
-];
-
-const DropdownComponent = () => {
-  const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
-
-  return (
-    <View style={styles.container}>
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: "red" }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? "Program" : "Please select your program"}
-        searchPlaceholder="Search"
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={(item) => {
-          setValue(item.value);
-          setIsFocus(false);
-        }}
-      />
-    </View>
-  );
-};
-
-export default DropdownComponent;
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 1,
-    paddingHorizontal: 40,
-    alignContent: "center",
-    justifyContent: "center",
-  },
-  dropdown: {
-    height: 60,
-    borderColor: "#671111",
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 8,
-  },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: "absolute",
-    backgroundColor: "red",
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    fontSize: 14,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
-});
-
-/* import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const data = [
   {
@@ -136,26 +30,36 @@ const data = [
     ],
   },
   { label: "Bachelor of Science in Psychology (BSP)", value: "7" },
-  { label: "Bachelor of Science in Criminology (BSCRIM)", value: "7" },
+  { label: "Bachelor of Science in Criminology (BSCRIM)", value: "8" },
   {
     label: "Bachelor of Science in Technology and Livelihood Education",
-    value: "8",
+    value: "9",
   },
 ];
 
 const DropdownComponent = () => {
   const [programValue, setProgramValue] = useState(null);
   const [subCategoryValue, setSubCategoryValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
 
   return (
     <View style={styles.container}>
       <Dropdown
-        style={styles.dropdown}
+        style={[styles.dropdown, isFocus && { borderColor: "#671111" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
         data={data}
+        search
+        maxHeight={hp(30)}
         labelField="label"
         valueField="value"
-        placeholder="Select Program"
+        placeholder={!isFocus ? "Program" : "Please select your program"}
+        searchPlaceholder="Search"
         value={programValue}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
         onChange={(item) => {
           setProgramValue(item.value);
           if (item.value !== "6") {
@@ -174,7 +78,7 @@ const DropdownComponent = () => {
           data={data.find((item) => item.value === "6").children[0].children}
           labelField="label"
           valueField="value"
-          placeholder="Select Sub Category"
+          placeholder="Major in"
           value={subCategoryValue}
           onChange={(item) => setSubCategoryValue(item.value)}
           renderCustomItem={(item) => (
@@ -197,8 +101,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   dropdown: {
-    width: 300,
+    width: wp(79),
     marginBottom: 10,
+    height: 60,
+    borderColor: "#671111",
+    borderWidth: 2,
+    borderRadius: 8,
+    paddingHorizontal: 8,
   },
   labelContainer: {
     flex: 1,
@@ -209,5 +118,18 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "400",
   },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
 });
- */
