913c0e64348d1482359a148b5fdf05830cdd62b8
John Henry C. Angas <angasjohnhenry@gmail.com>
2024-05-17 15:36:48 +0800

    terms and privacy policies

--- screens/PrivacyPolicy.js
+++ screens/PrivacyPolicy.js
@@ -1,12 +1,170 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-export default function PrivacyPolicy() {
+import ButtonComponent from "../components/ButtonComponent";
+export default function TermsAndConditions({ navigation }) {
   return (
-    <View>
-      <Text>PrivacyPolicy</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/img/logo-no-background.png")}
+            resizeMode="contain"
+          />
+        </View>
+
+        <Text style={styles.titleStyle}>Privacy Policy</Text>
+        <Text style={styles.pointer}>{"\t"}1. Introduction </Text>
+        <View style={styles.contents}>
+          {/* Terms and Conditions */}
+          <Text style={styles.contentStyle}>
+            {"\n"}
+            Athenaeum: ECC Library Management System is committed to protecting
+            your privacy. This privacy policy describes how our system collects
+            uses, and shares your information when you use and interact with our
+            services and applies both students and faculty members. We will not
+            use your data in accordance with the statutory data protection
+            regulations and this data protection declaration.
+            {"\n"}
+            {"\n"}
+            We respect your privacy and want to protect your personal
+            information. This privacy policy explains how we collect, use, and
+            disclose your information.
+            {"\n"}
+            {"\n"}
+          </Text>
+        </View>
+
+        <Text style={styles.pointer}>{"\t"}2. Information We Collect </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          We collect personal information such as your name, email address, and
+          student number when you register for an account. We also collect
+          information about the books you borrow, your borrowing history, and
+          any fines or fees associated with your account.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <Text style={styles.pointer}>
+          {"\t"}3. How We Use Your Information{" "}
+        </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          We use your information to manage your library account, provide you
+          with services, communicate with you, and improve our system. We do not
+          sell or share your personal information with third parties for
+          marketing purposes.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <Text style={styles.pointer}>
+          {"\t"}4. How We Protect Your Information{" "}
+        </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          We implement a variety of security measures to maintain the safety of
+          your personal information. Your personal information is contained
+          behind secured networks and is only accessible by a limited number of
+          persons who have special access rights to such systems.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <Text style={styles.pointer}>{"\t"}5. Your Rights </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          You have the right to access, correct, your personal information at
+          any time. Please contact us if you wish to exercise these rights.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <Text style={styles.pointer}>
+          {"\t"}6. Changes to This Privacy Policy{" "}
+        </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          We may update our Privacy Policy from time to time. We will notify you
+          of any changes by posting the new Privacy Policy on this page.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <Text style={styles.pointer}>{"\t"}7. Contact Us </Text>
+        <Text style={styles.contentStyle}>
+          {"\n"}
+          If you have any questions about this Privacy Policy you can:
+          {"\n"}
+          {"\n"}
+          Email us at:
+          {"\n"}
+          Athenaeum.ecc@gmail.comDevelopers
+          {"\n"}
+          {"\n"}
+          <Text>or</Text>
+          {"\n"}
+          {"\n"}
+          Contact us at 0926-708-1232
+        </Text>
+        <View style={styles.buttonContainer}>
+          <ButtonComponent
+            text="Accept"
+            /*  onPress={() => navigation.navigate("First Time Install")} */
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoContainer: {
+    height: hp(12),
+    width: wp(80),
+    alignSelf: "center",
+  },
+  logo: {
+    height: hp(40),
+    width: wp(80),
+    bottom: hp(15),
+    alignSelf: "center",
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  contents: {
+    width: wp(100),
+    gap: 5,
+  },
+  contentStyle: {
+    fontSize: 15,
+
+    width: wp(90),
+    alignSelf: "center",
+    alignContent: "center",
+    textAlign: "justify",
+  },
+  pointer: {
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    marginTop: hp(3),
+    height: hp(10),
+  },
+});
--- screens/TermsAndConditions.js
+++ screens/TermsAndConditions.js
@@ -1,12 +1,158 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-export default function TermsAndConditions() {
+import ButtonComponent from "../components/ButtonComponent";
+export default function TermsAndConditions({ navigation }) {
   return (
-    <View>
-      <Text>TermsAndConditions</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../images/logo_bago.png")}
+            resizeMode="contain"
+          />
+        </View>
+
+        <Text style={styles.titleStyle}>Terms and Conditions</Text>
+        <View style={styles.contents}>
+          {/* Terms and Conditions */}
+          <Text style={styles.contentStyle}>
+            Athenaeum developers hereby strictly reminds you to read and
+            understand carefully to our terms and conditions. When reading,
+            please focus especially on these factors. You read the agreement and
+            then decide whether or not to accept it.
+          </Text>
+
+          <Text style={styles.titleStyle}>
+            About the Athenaeum Mobile Application
+          </Text>
+          <Text style={styles.contentStyle}>
+            Athenaeum Mobile Application is developed due to the reason that the
+            developer looks for a solution in order to provide and produce a
+            digital library and attendance system to the library of EARIST –
+            Cavite Campus. In which, students from ECC may view the available
+            books from the library online, and will also make a transaction
+            online such as reserving and borrowing books. Such these
+            transactions there are corresponding policies and penalties over the
+            actions of every user.
+          </Text>
+          <Text style={styles.titleStyle}>
+            Terms and Conditions on Creating User Account
+          </Text>
+
+          {/* Account Creation Policy */}
+          <Text style={styles.titleStyle}>Creating an Account</Text>
+          <Text style={styles.pointer}> 1.1 Registration: </Text>
+          <Text style={styles.contentStyle}>
+            During the registration process the all the credentials asked should
+            be filled out honestly to the needed field. Also, the institutional
+            Gmail should be used.
+          </Text>
+          <Text style={styles.pointer}> 1.2 Logging in: </Text>
+          <Text style={styles.contentStyle}>
+            During the logging in process, the two needed filled should be
+            thrice try only, once it already reaches it logging in tries, it
+            will be restricted and locked.
+          </Text>
+          <Text style={styles.pointer}> 1.3 Account Tab: </Text>
+          <Text style={styles.contentStyle}>
+            In account tab, the user will be able to edit his details
+            accordingly.
+          </Text>
+          {/* Book Transactions */}
+          <Text style={styles.titleStyle}>
+            Terms and Conditions on Book Transactions
+          </Text>
+          <Text style={styles.titleStyle}>Borrowing</Text>
+          <Text style={styles.pointer}> 1.1 Overdue: </Text>
+          <Text style={styles.contentStyle}>
+            The overdue books will be notified on the status field in the user
+            account.
+          </Text>
+
+          <Text style={styles.pointer}> 1.2 Lost Books or Damage books: </Text>
+          <Text style={styles.contentStyle}>
+            The lost books should be replaced by a book purchased book amount or
+            the similar book.
+          </Text>
+          <Text style={styles.pointer}> 1.3 Replacement: </Text>
+          <Text style={styles.contentStyle}>
+            The replacement of the book is a day after the book was issued.
+          </Text>
+          <Text style={styles.titleStyle}>Reservation</Text>
+          <Text style={styles.pointer}> 2.1 Reservation Time: </Text>
+          <Text style={styles.contentStyle}>
+            The user who will make a reservation will reserve during the library
+            hours. All transactions should occur during library hours.
+          </Text>
+          <Text style={styles.pointer}> 2.2 Reservation Status: </Text>
+          <Text style={styles.contentStyle}>
+            The user who will make a reservation will wait until the admin will
+            accept or decline the user reservation.
+          </Text>
+          <Text style={styles.pointer}> 2.3 Reservation Qualification: </Text>
+          <Text style={styles.contentStyle}>
+            Updated Certificate of Registration in ECC.
+          </Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <ButtonComponent
+            text="Accept"
+            onPress={() => navigation.navigate("First Time Install")}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoContainer: {
+    height: hp(12),
+    width: wp(80),
+    alignSelf: "center",
+  },
+  logo: {
+    height: hp(40),
+    width: wp(80),
+    bottom: hp(15),
+    alignSelf: "center",
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  contents: {
+    width: wp(100),
+    gap: 5,
+  },
+  contentStyle: {
+    fontSize: 15,
+
+    width: wp(90),
+    alignSelf: "center",
+    alignContent: "center",
+    textAlign: "justify",
+  },
+  pointer: {
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    marginTop: hp(3),
+    height: hp(10),
+  },
+});
