0dd7ff889fa1499e47fa120edb188cdcb3e591c2
BabuMome <angasjohnhenry@gmail.com>
2024-05-23 15:47:19 +0800

    added some dummy data

--- data_samples/searchData.js
+++ data_samples/searchData.js
@@ -4,4 +4,24 @@ export const search_books = [
     book_title: "Ang Mama at Papa Mo",
     book_author: "J.K Rowling",
   },
+  {
+    id: 2,
+    book_title: "ZZZZZZZZZZ",
+    book_author: "J.K Rowling",
+  },
+  {
+    id: 3,
+    book_title: "General",
+    book_author: "Townes",
+  },
+  {
+    id: 4,
+    book_title: "Henricakes",
+    book_author: "mome",
+  },
+  {
+    id: 5,
+    book_title: "Ericzon",
+    book_author: "boss r",
+  },
 ];
