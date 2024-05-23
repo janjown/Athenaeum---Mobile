df022fefd924ccc7e5393a4a004ceb4df1c804b0
BabuMome <angasjohnhenry@gmail.com>
2024-05-23 15:46:01 +0800

    added new component

---
+++ components/ButtonNormal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+
+export default function ButtonNormal({
+  text,
+  onPress,
+  navigation,
+  style,
+  backgroundColor,
+}) {
+  return (
+    <TouchableOpacity
+      style={[styles.buttonContainer, style]}
+      onPress={onPress}
+      activeOpacity={0.7}
+    >
+      <View style={[styles.button, backgroundColor && { backgroundColor }]}>
+        <Text style={styles.buttonText}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderRadius: 25,
+    width: wp(45),
+    height: hp(5),
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
