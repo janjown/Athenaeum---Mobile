ab19ba1e83704bda78c1e9a3d9e2cf000309af72
BabuMome <angasjohnhenry@gmail.com>
2024-05-27 12:40:56 +0800

    added some visuals and re-render fonts

--- screens/Module.js
+++ screens/Module.js
@@ -4,36 +4,85 @@ import {
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import React from "react";
+import { useFonts } from "expo-font";
+import LottieView from "lottie-react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 // Components
 import ButtonComponent from "../components/ButtonComponent";
+
 export default function Module({ navigation }) {
-  return (
-    <SafeAreaView style={style.container}>
-      <View style={style.centered}>
-        <Image style={style.logo} source={require("../assets/img/logo.png")} />
-        <Text style={style.questionText}>Are you a...?</Text>
+  const [fontsLoaded] = useFonts({
+    "CreteRound-Regular": require("../assets/fonts/CreteRound-Regular.ttf"),
+    "Figtree-VariableFont": require("../assets/fonts/Figtree-VariableFont_wght.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#7D0707" />
       </View>
-      <View style={style.buttonContainer}>
-        <View style={style.studentContainer}>
+    );
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centered}>
+        <Image style={styles.logo} source={require("../assets/img/logo.png")} />
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            color: "#671111",
+            fontFamily: "CreteRound-Regular",
+          }}
+        >
+          Are you a...?
+        </Text>
+      </View>
+      <View style={styles.animationContainer}>
+        <Animated.View entering={FadeInDown.duration(700)}>
+          <LottieView
+            style={styles.animationStudent}
+            source={require("../assets/animations/student.json")}
+            autoPlay
+            loop
+          />
+        </Animated.View>
+        <Animated.View entering={FadeInDown.duration(700)}>
+          <LottieView
+            style={styles.animationFaculty}
+            source={require("../assets/animations/faculty.json")}
+            autoPlay
+            loop
+          />
+        </Animated.View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.studentContainer}>
           <ButtonComponent
             text={"Student"}
             onPress={() => navigation.navigate("Student Registration")}
           />
         </View>
-        <View style={style.facultyContainer}>
+        <View style={styles.facultyContainer}>
           <ButtonComponent
             text={"Faculty"}
             onPress={() => navigation.navigate("Faculty Registration")}
           />
         </View>
       </View>
-      <Text style={style.school}>EARIST - Cavite Campus</Text>
+      <Text
+        style={{
+          fontFamily: "CreteRound-Regular",
+        }}
+      >
+        EARIST - Cavite Campus @2024 Athenaeum
+      </Text>
     </SafeAreaView>
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     flex: 1,
@@ -51,32 +100,39 @@ const style = StyleSheet.create({
     height: hp(30),
     resizeMode: "contain",
   },
-  questionText: {
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#671111",
-    fontFamily: "CreteRound-Regular",
-  },
   buttonContainer: {
     flex: 1,
     flexDirection: "row",
     gap: wp(5),
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: hp(10),
+    marginBottom: hp(9),
   },
   studentContainer: {
     flexDirection: "column",
   },
   animationContainer: {
-    flexDirection: "row",
+    flexDirection: "column",
   },
-  /*  animations: {
-    height: hp(100),
-    width: wp(100),
-  }, */
   facultyContainer: {
     flexDirection: "column",
+  },
+  animationStudent: {
+    width: wp(32),
+    right: wp(0),
+    height: hp(20),
+  },
+  animationFaculty: {
+    width: wp(32),
+    left: wp(2),
+    height: hp(21.5),
+  },
+  animationContainer: {
+    position: "absolute",
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: wp(17),
   },
 });
