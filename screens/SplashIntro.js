1b0c93a4eb0448a71cbe5028c150e11e34a72204
BabuMome <angasjohnhenry@gmail.com>
2024-06-03 17:09:16 +0800

    reworked intro

--- screens/SplashIntro.js
+++ screens/SplashIntro.js
@@ -1,9 +1,13 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
 import LottieView from "lottie-react-native";
 import { useFonts } from "expo-font";
-import Animated, { FadeInDown } from "react-native-reanimated";
+import Animated, {
+  BounceInLeft,
+  BounceInRight,
+  FadeInDown,
+} from "react-native-reanimated";
 import { useNavigation } from "@react-navigation/native";
 import {
   widthPercentageToDP as wp,
@@ -13,7 +17,7 @@ import {
 export default function SplashIntro() {
   const navigation = useNavigation();
   useEffect(() => {
-    setTimeout(() => navigation.navigate("Login"), 5000);
+    setTimeout(() => navigation.navigate("Login"), 7000);
   }, []);
 
   let [fontsLoaded] = useFonts({
@@ -27,6 +31,11 @@ export default function SplashIntro() {
         style={styles.container}
         className="flex-1 justify-center items-center dark:bg-neutral-900"
       >
+        <Animated.Image
+          entering={FadeInDown.delay(1200).springify()}
+          style={styles.logo}
+          source={require("../assets/img/logo.png")}
+        />
         {/* Animated Title Text */}
         <Animated.Text
           entering={FadeInDown.delay(1600).springify().damping(7).mass(3)}
@@ -35,6 +44,22 @@ export default function SplashIntro() {
         >
           ATHENAEUM
         </Animated.Text>
+        <View style={styles.inarte}>
+          <Animated.Text
+            entering={BounceInLeft.delay(1800)}
+            style={styles.appTitle2}
+            className="font-bold absolute"
+          >
+            RE
+          </Animated.Text>
+          <Animated.Text
+            entering={BounceInRight.delay(2000)}
+            style={styles.appTitle3}
+            className="font-bold absolute"
+          >
+            WORK
+          </Animated.Text>
+        </View>
 
         {/* Lottie Animation */}
         <LottieView
@@ -57,16 +82,37 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#FFFFFF",
   },
+  logo: {
+    top: hp(50),
+    height: hp(30),
+    width: wp(50),
+    marginTop: hp(20),
+  },
   animation: {
-    bottom: hp(3),
-    height: hp(63),
+    top: hp(5),
+    height: hp(73),
     width: wp(100),
-    marginBottom: hp(20),
+    marginBottom: hp(70),
+  },
+  inarte: {
+    flexDirection: "row",
   },
   appTitle: {
-    top: hp(40),
+    top: hp(55),
     fontSize: 50,
     color: "#671111",
+    fontFamily: "CreteRound-Regular",
+  },
+  appTitle2: {
+    top: hp(55),
+    fontSize: 40,
+    color: "#cdcf65",
+    fontFamily: "CreteRound-Regular",
+  },
+  appTitle3: {
+    top: hp(55),
+    fontSize: 40,
+    color: "#cdcf65",
     fontFamily: "CreteRound-Regular",
   },
 });
